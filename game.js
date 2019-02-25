6c90d6c136f7572fb5bcb490b1955b2315d1895a
Nikita Osipov <nikitaosipov.51@gmail.com>
2019-02-25 13:03:50 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,67 @@
+var config = {
+    type: Phaser.WebGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#aa7d65',
+    scene: {
+        preload: preload,
+        create: create,
+//        update: update
+    }
+}
+
+var game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image('body', 'assets/games/snake/body.png');
+    this.load.image('food', 'assets/games/snake/food.png');
+}
+
+function create () {
+
+    var cursors = this.input.keyboard.createCursorKeys();
+    var UP = 0;
+    var DOWN = 1;
+    var LEFT = 2;
+    var RIGHT = 3;
+
+    var foodsprite = this.add.image('food');
+    var snakesprite = this.add.image('body');
+
+    var Snake = new Phaser.Class({
+
+        create: function(scene, x, y) {
+            this.coordinates = new Phaser.Geom.Point(x, y);
+            this.head = snakesprite;
+            this.head.x = x;
+            this.head.y = y;
+            this.position = RIGHT;
+            this.direction = RIGHT;
+            this.moveTime = 0;
+        },
+
+        update: function(time) {
+
+        }
+    });
+    var snake = new Snake(this, 16, 16);
+}
+
+function move () {
+
+}
+
+  function update () {
+  if (cursors.up.isDown) {
+
+  }
+  if (cursors.down.isDown) {
+
+  }
+  if (cursors.left.isDown) {
+
+  }
+  if (cursors.right.isDown) {
+
+  }
+  }
