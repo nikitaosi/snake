c3b3e68768d13c49a98efa1239a86bd4e8bbcc5f
Nikita Osipov <nikitaosipov.51@gmail.com>
2019-02-26 12:19:57 +0300

    snake walking down the street

--- game.js
+++ game.js
@@ -10,13 +10,17 @@ var config = {
     }
 }
 
-var game = new Phaser.Game(config);
-
-var cursors = game.input.keyboard.createCursorKeys();
 var UP = 0;
 var DOWN = 1;
 var LEFT = 2;
 var RIGHT = 3;
+var SPEED = 100;
+
+var game = new Phaser.Game(config);
+var moveTime = 0;
+var step = 16;
+var direction = RIGHT;
+
 
 function preload () {
     this.load.image('body', 'assets/games/snake/body.png');
@@ -24,45 +28,52 @@ function preload () {
 }
 
 function create () {
-    var foodsprite = this.add.image('food');
-    var snakesprite = this.add.image(0,0,'body');
-
-    var Snake = new Phaser.Class({
-
-        initialize:
-
-        function Snake(scene, x, y) {
-            this.head = snakesprite;
-            this.head.x = x;
-            this.head.y = y;
-            this.position = RIGHT;
-            this.direction = RIGHT;
-            this.moveTime = 0;
-        },
-
-    //    update: function(time) {
-    //    }
-    });
+    this.cursors = this.input.keyboard.createCursorKeys();
+    food = this.add.image(512,256,'food');
+    snake = this.add.image(256,256,'body');
+    snake.setOrigin(0); food.setOrigin(0);
 }
 
-var snake = new Snake(this, 256, 256);
+function update (time) {
+if (moveTime<=time) {
+  move();
+   moveTime = time + SPEED;
+}
+
+
+
+if (this.cursors.up.isDown) {
+  direction = UP;
+}
+if (this.cursors.down.isDown) {
+  direction = DOWN;
+}
+if (this.cursors.left.isDown) {
+  direction = LEFT;
+}
+if (this.cursors.right.isDown) {
+  direction = RIGHT;
+}
 
 function move () {
-//   snake.setPosition(snake.head.x+1, snake.head.y);
+  switch (direction) {
+      case UP:
+          snake.y -= step;
+          if(snake.y<0){snake.y = 584}
+          break;
+      case DOWN:
+          snake.y += step;
+          if(snake.y>584){snake.y = 0}
+          break;
+      case LEFT:
+          snake.x -= step;
+          if(snake.x<0){snake.x = 784}
+          break;
+      case RIGHT:
+          snake.x += step;
+          if(snake.x>784){snake.x = 0}
+          break;
+  }
 }
 
-  function update (time, delta) {
-    move();
-  if (cursors.up.isDown) {
-
-  }
-  if (cursors.down.isDown) {
-
-  }
-  if (cursors.left.isDown) {
-
-  }
-  if (cursors.right.isDown) {
-
-  }
   }
